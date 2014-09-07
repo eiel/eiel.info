b00ab3e67a68b4b68c43eba97892daa1dad8347a
Tomohiko Himura <eiel.hal@gmail.com>
2014-09-08 01:30:02 +0900

    Add Rack::Deflater

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module Eiel
 
     # For not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.use Rack::Deflater
   end
 end
