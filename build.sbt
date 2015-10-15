13e6c1c44eabc7c67cb2b729cd84871d3d0ea2fc
Tomohiko Himura <eiel.hal@gmail.com>
2015-10-15 23:59:54 +0900

    initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+enablePlugins(ScalaJSPlugin)
+
+name := "eiel.info"
+
+scalaVersion := "2.11.7"
---
+++ src/main/scala/info/eiel/Main.scala
@@ -0,0 +1,9 @@
+package info.eiel
+
+import scala.scalajs.js.JSApp
+
+object Main extends JSApp {
+  def main(): Unit = {
+    println("Hello, World")
+  }
+}
