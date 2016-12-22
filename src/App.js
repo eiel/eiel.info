8d5b8d62fd21d3e90f5616d5e5704241653e6aa7
Tomohiko Himura <eiel.hal@gmail.com>
2016-12-22 09:54:30 +0900

    挨拶を完成

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import Avatar from 'material-ui/Avatar';
+import Dialog from 'material-ui/Dialog';
+import IconButton from 'material-ui/IconButton';
+import Paper from 'material-ui/Paper';
 
-import {} from './firebaseInit'
+import moment from 'moment';
+moment.locale('ja');
+
 import firebase from 'firebase/app'
 import {} from 'firebase/auth'
+import {} from 'firebase/database'
 
-window.firebase = firebase;
 
 import './App.css';
 
-let user;
+let config = {
+    apiKey: "AIzaSyDpbq9hDmGyrzYJcGlvwrjJXL-01No3XNA",
+    authDomain: "eiel-91077.firebaseapp.com",
+    databaseURL: "https://eiel-91077.firebaseio.com",
+    storageBucket: "eiel-91077.appspot.com",
+    messagingSenderId: "356361494886"
+};
+
+firebase.initializeApp(config);
+
 
 let provider = new firebase.auth.GoogleAuthProvider();
 let authGoogle = () => {
@@ -19,7 +35,7 @@ let authGoogle = () => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         let token = result.credential.accessToken;
         // The signed-in user info.
-        user = result.user;
+        let user = result.user;
         // ...
     }).catch(function(error) {
         // Handle Errors here.
@@ -38,37 +54,132 @@ class App extends Component {
         super();
         this.state = {
             user: false,
+            signOutDialog: false,
+            messages: []
         };
 
-        this.handleSignOut = () => {
-            firebase.auth().signOut()
-                .then(() => { this.setState({user: null}); })
-                .catch((err) => { console.log(err); });
-        };
-
-        this.handleSignIn = () => {
-            authGoogle();
-        };
+        setInterval(() => {
+            this.setState({messages: this.state.messages});
+        }, 500);
 
         firebase.auth().onAuthStateChanged((user) => {
-            console.log(user);
             this.setState({user});
+        });
+
+        firebase.database().ref('messages').on('value', (snapshot) => {
+            let values = snapshot.val();
+            if (values) {
+                let keys = Object.keys(values);
+                let length = keys.length;
+
+                let messages = [];
+                for (let key of keys){
+                    let message = values[key];
+                    messages.unshift(message);
+                }
+                this.setState({messages});
+
+                let updates = {};
+                let deleteKeys = keys.splice(0,length-5);
+                for (let key of deleteKeys) {
+                    updates[key] = null;
+                }
+                firebase.database().ref('messages').update(updates);
+            }
         });
     }
 
+    handleOpenSignOutDialog = () => {
+        this.setState({signOutDialog: true});
+    }
+
+    handleCloseSignOutDialog = () => {
+        this.setState({signOutDialog: false});
+    }
+
+    handleSignOut = () => {
+        firebase.auth().signOut()
+            .then(this.handleCloseSignOutDialog)
+            .then(() => { this.setState({user: null}); })
+            .catch((err) => { console.log(err); });
+    }
+
+    handleSignIn = () => {
+        authGoogle();
+    };
+
     auth() {
         if (this.state.user) {
-            return <FlatButton label={this.state.user.displayName} onClick={this.handleSignOut}/>;
+            return <div onClick={this.handleOpenSignOutDialog}>
+                <Avatar src={this.state.user.photoURL} onClick={this.handleOpenSignOutDailog}/>
+            </div>
         } else {
             return <FlatButton label="Sign In" onClick={this.handleSignIn}/>;
         }
     }
 
+    addMessage(message) {
+        const key = firebase.database().ref('messages').push().key;
+        firebase.database().ref(`messages/${key}`).set({created_at: new Date().getTime(), message: message, photoURL: this.state.user.photoURL})
+    }
+
     render() {
+        const handleSignOutWithClose = () => {
+            this.handleSignOut();
+        };
+        const actions = [
+            <FlatButton
+                label="キャンセル"
+                onClick={this.handleCloseSignOutDialog}
+            />,
+            <FlatButton
+                label="サインアウト"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.handleSignOut}
+            />,
+        ];
+
+        const userMessage = (() => {
+            if (this.state.user === null) {
+                return <Paper style={{margin: "10px", padding: "8px"}} zDepth={3}>
+                    サインインすると挨拶ができます
+                </Paper>
+            } {
+                return <div style={{"text-align": "center", margin: "10px"}}>
+                    <RaisedButton label="挨拶をする" style={{width:200,margin: "auto", padding: "0"}} onClick={() => { this.addMessage(`${this.state.user.displayName}さんが挨拶しました。`) }}/>
+                </div>
+            }
+        })();
+
+        const message = (() => {
+            return this.state.messages.map((message) => {
+                return <Paper key={message.created_at} style={{margin: "10px", padding: "8px"}} zDepth={2}>
+                    <Avatar src={message.photoURL} style={{"margin-right": "10px"}}/>
+                    {message.message} <span style={{color: "#999", "font-sizei": -1}}>{moment(message.created_at).fromNow()}</span>
+                </Paper>
+            });
+        })();
         return (
             <MuiThemeProvider>
                 <div>
                     <AppBar title="eiel.info" iconElementRight={this.auth()} />
+
+                    {userMessage}
+                    {message}
+
+
+
+                    <IconButton className="fa fa-github" />
+
+                    <Dialog
+                        title="サインアウトしますか?"
+                        actions={actions}
+                        modal={true}
+                        open={this.state.signOutDialog}
+                        onRequestClose={this.handleCloseSignOutDialog}
+                    >
+                    </Dialog>
                 </div>
             </MuiThemeProvider>
         );
--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,5 +1,4 @@
 import firebase from 'firebase/app';
-import {} from 'firebase/auth';
 
 let config = {
     apiKey: "AIzaSyDpbq9hDmGyrzYJcGlvwrjJXL-01No3XNA",
@@ -8,4 +7,5 @@ let config = {
     storageBucket: "eiel-91077.appspot.com",
     messagingSenderId: "356361494886"
 };
-firebase.initializeApp(config);
+
+export default firebase.initializeApp(config);
