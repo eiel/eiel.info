c0d2d6ba5af419815af90461c63a0650c0d2f3c4
Tomohiko Himura <eiel.hal@gmail.com>
2017-08-10 14:03:42 +0900

    minified

---
+++ src/components/Site.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const SNSList = [
+    {
+        href: "https://github.com/eiel",
+        label: "GitHub",
+    },
+    {
+        href: "https://twitter.com/eielh",
+        label: "Twitter",
+    },
+    {
+        href: "https://www.facebook.com/eielh",
+        label: "Facebook",
+    },
+    {
+        href: "http://qiita.com/eielh",
+        label: "Qiita",
+    },
+    {
+        href: "https://plus.google.com/+himuratomohiko",
+        label: "Google+",
+    },
+    {
+        href: "https://www.flickr.com/photos/43922152@N04/",
+        label: "Flickr",
+    }
+];
+
+const Service = ({url, title}) => {
+  return <li>
+    <a href={url}>{title}</a>
+  </li>
+}
+
+const ServiceList = ({list}) => {
+  const content = list.map(({title, url}, index) => {
+    return <Service key={index} title={title} url={url} />
+  })
+
+  return <ul>
+    {content}
+  </ul>
+}
+
+export default ({title}) => {
+  const list = SNSList.map(({href, label}) => { return { title: label, url: href } });
+  return pug`
+    div
+      h1= title
+      ServiceList(list=list)
+  `
+}
--- src/generate.js
+++ src/generate.js
@@ -1,16 +1,25 @@
+// @flow
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import pug from 'pug';
 
-const content = ReactDOMServer.renderToStaticMarkup(
-  React.createElement("div", {}, "Hello, World")
-)
+import Site from './components/Site'
+
 const fn = pug.compile(`doctype html
 head
+  meta(charset="utf-8")
+  meta(http-equiv="x-ua-compatible" content="ie=edge")
   title= title
+  meta(name="description" content="")
+  meta(name="viewport" content="width=device-width, initial-scale=1")
+  link(rel="apple-touch-icon" href="apple-touch-icon.png")
 body !{content}
 `);
-const title = "eiel.info";
-const html = fn({content, title});
+const title = "eiel.info"
+const description = "Tomohiko Himura's Website. He a.k.a. eiel"
+const content = ReactDOMServer.renderToStaticMarkup(
+  React.createElement(Site, {"title": title})
+)
+const html = fn({content, title, description});
 
 console.log(html);
