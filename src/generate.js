a086b86d912f85efd0462ea4bcc36fefbc2788c3
Tomohiko Himura <eiel.hal@gmail.com>
2017-08-10 11:53:06 +0900

    static render

---
+++ src/generate.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+console.log(
+  ReactDOMServer.renderToStaticMarkup(
+    React.createElement("div", {}, "Hello, World")
+  )
+);
