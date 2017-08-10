f39102943c5578e51a6315977dc4d1840557eb6a
Tomohiko Himura <eiel.hal@gmail.com>
2017-08-10 12:07:55 +0900

    setup pug

--- src/generate.js
+++ src/generate.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import pug from 'pug';
 
-console.log(
-  ReactDOMServer.renderToStaticMarkup(
-    React.createElement("div", {}, "Hello, World")
-  )
-);
+const content = ReactDOMServer.renderToStaticMarkup(
+  React.createElement("div", {}, "Hello, World")
+)
+const fn = pug.compile(`doctype html
+head
+  title= title
+body !{content}
+`);
+const title = "eiel.info";
+const html = fn({content, title});
+
+console.log(html);
