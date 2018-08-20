53c6dcb781507b5d02cc651219ae12dd5c41d4ac
Tomohiko Himura <eiel.hal@gmail.com>
2018-08-21 00:28:54 +0900

    feat: 編集ボタンを追加

--- src/components/pages/GreenRibbon.js
+++ src/components/pages/GreenRibbon.js
@@ -2,12 +2,16 @@
 import React from 'react';
 import DefaultTemplate from '../templates/DefaultTemplate';
 
-const title = 'グリーンリボンを応援する会 - eiel.info';
-const description = "Tomohiko Himura's Website. He a.k.a. eiel";
+const title = '移植医療のために活動する人たちを応援する会';
+const description =
+  'グリーンリボンというシンボルを活用して、移植医療のために活動する人たちを応援するためのページ';
+const src = 'GreenRibbon.js';
+const href = `https://github.com/eiel/eiel.info/blob/master/src/components/pages/${src}`;
 
 const GreenRibbon = () => (
   <DefaultTemplate {...{ title, description }}>
     <h1>{title}</h1>
+    <a href={href}>編集</a>
   </DefaultTemplate>
 );
 
