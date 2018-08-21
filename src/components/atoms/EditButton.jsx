f93a40b560c61b780965e8eaa9108c831d0ff66b
Tomohiko Himura <eiel.hal@gmail.com>
2018-08-21 22:20:27 +0900

    fix: 変種するに文言変更

--- src/components/atoms/EditButton.jsx
+++ src/components/atoms/EditButton.jsx
@@ -4,5 +4,5 @@ import React from 'react';
 const href = src =>
   `https://github.com/eiel/eiel.info/blob/master/src/components/pages/${src}`;
 
-export const EditButton = ({ filename }) => <a href={href(filename)}>編集</a>;
+export const EditButton = ({ filename }) => <a href={href(filename)}>編集する</a>;
 export default EditButton;
