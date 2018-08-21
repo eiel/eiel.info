a3386510cb7962cb591f5cfbbfdf9b17cd6fa2d5
Tomohiko Himura <eiel.hal@gmail.com>
2018-08-21 21:04:42 +0900

    style: code format

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { routing } from './routing';
 // import './index.css';
 
-//const Page = routing[location.pathname];
+// const Page = routing[location.pathname];
 // const Page = routing[location.hash.replace('#', '')] || routing['/'];
 // const Page = routing[location.hash.replace('#', '')] || routing['/'];
 // eslint-disable-next-line no-undef
