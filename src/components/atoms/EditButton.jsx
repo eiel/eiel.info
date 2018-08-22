48b567ec9b97cf3946650a7a6d54b0a89423761b
Tomohiko Himura <eiel.hal@gmail.com>
2018-08-22 23:56:30 +0900

    feat: EditButton

--- src/components/atoms/EditButton.jsx
+++ src/components/atoms/EditButton.jsx
@@ -1,8 +1,13 @@
 // @flow
 import React from 'react';
+import './EditButton.css';
 
 const href = src =>
   `https://github.com/eiel/eiel.info/blob/master/src/components/pages/${src}`;
 
-export const EditButton = ({ filename }) => <a href={href(filename)}>編集する</a>;
+export const EditButton = ({ filename }: { filename: string }) => (
+  <div styleName="button">
+    <a href={href(filename)}>編集する</a>
+  </div>
+);
 export default EditButton;
