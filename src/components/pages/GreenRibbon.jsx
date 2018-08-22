939483170b5afe798d04ae519ef89db598f3a37e
Tomohiko Himura <eiel.hal@gmail.com>
2018-08-23 00:26:15 +0900

    refactor: データを分離

--- src/components/pages/GreenRibbon.jsx
+++ src/components/pages/GreenRibbon.jsx
@@ -4,38 +4,13 @@ import Markdown from 'react-markdown';
 import DefaultTemplate from '../templates/DefaultTemplate';
 import { EditButton } from '../atoms/EditButton';
 import './GreenRibbon.css';
+import { Links } from '../../resources/GreenRibbon/Links';
+import type { Link } from '../../resources/GreenRibbon/Links';
 
 const title = '移植医療への理解を広げる会';
 const description =
   'グリーンリボンをシンボルに、移植医療のために活動する人たちを応援するためのページです。';
 const filename = 'GreenRibbon.jsx';
-
-type Link = {
-  url: string,
-  name: string,
-  description: string
-};
-
-const links: Link[] = [
-  {
-    name: 'グリーンリボンキャンペーン',
-    url: 'http://www.green-ribbon.jp/',
-    description:
-      'わかりやすい表現で、臓器医療への理解を深め、意思表示を促すためのキャンペーンサイトです。かわいい検定があり、検定に合格するとグリーンリボンのピンバッジをもらうことができます。ゆるふわな感じなのでぜひ挑戦してみてください。'
-  },
-  {
-    name: '(公社)日本臓器移植ネットワーク',
-    url: 'https://www.jotnw.or.jp/',
-    description:
-      'JOTと略される。臓器提供を希望する人から、移植を受けたい人へ、臓器を提供する橋渡しをしている重要な組織。'
-  },
-  {
-    name: 'NPO法人グリーンリボン推進協会',
-    url: 'https://www.greenribbonac.com/',
-    description:
-      '移植医療が期待したほど普及が進まないのをみて、立ち上がった市民団体。近年広島支部の活動が活発。'
-  }
-];
 
 const LinkComponent = ({ url, name, description }: Link) => (
   <li>
@@ -59,10 +34,10 @@ const lead = `
 
 const GreenRibbon = ({ isDev }: { isDev: boolean }) => (
   <DefaultTemplate {...{ title, description, isDev }}>
-    <h1 styleName="title">移植医療への理解を広げる会</h1>
+    <h1 styleName="title">{title}</h1>
     <Markdown source={lead} />
     <h1 styleName="h1">リンク</h1>
-    <ul styleName="site">{links.map(LinkComponent)}</ul>
+    <ul styleName="site">{Links.map(LinkComponent)}</ul>
     <EditButton {...{ filename }} />
   </DefaultTemplate>
 );
