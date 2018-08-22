dcc91c3546420ee02be37e646a3e18cbb45b40c2
Tomohiko Himura <eiel.hal@gmail.com>
2018-08-23 00:31:53 +0900

    配色を修正

--- src/components/atoms/EditButton.jsx
+++ src/components/atoms/EditButton.jsx
@@ -6,8 +6,8 @@ const href = src =>
   `https://github.com/eiel/eiel.info/blob/master/src/components/pages/${src}`;
 
 export const EditButton = ({ filename }: { filename: string }) => (
-  <div styleName="button">
-    <a href={href(filename)}>編集する</a>
-  </div>
+  <a styleName="button" href={href(filename)}>
+    <span>このページを編集する</span>
+  </a>
 );
 export default EditButton;
