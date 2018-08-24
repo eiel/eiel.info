4bf920ed4f3e1f98ae7e2bfb8721fc95cb3269bb
Tomohiko Himura <eiel.hal@gmail.com>
2018-08-24 23:46:08 +0900

    chore: add jest-raw-loader

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
+    '\\.md$': 'jest-raw-loader',
     '\\.(css|less)$': 'identity-obj-proxy'
   }
 };
